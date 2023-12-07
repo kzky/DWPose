5d8ba1d07af81f50697973bb3884efe3b296a825
Kazuki.Yoshiyama <Kazuki.Yoshiyama@sony.com>
2023-12-07 10:35:26 +0000

    Input as image list

--- ControlNet-v1-1-nightly/inference.py
+++ ControlNet-v1-1-nightly/inference.py
@@ -7,25 +7,17 @@ import numpy as np
 import os
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--input_image", required=True)
+parser.add_argument("--input_image", nargs="+", required=True)
 args = parser.parse_args()
-
-if os.path.isdir(args.input_image):
-    input_images = sorted(glob.glob(f"{args.input_image}/*"))
-    dirpath = f"{Path(args.input_image).parent}/pose_img"
-    os.makedirs(dirpath, exist_ok=True)
-else:
-    input_images = [args.input_image]
 
 dwprocessor = DWposeDetector()
 
-for input_image in input_images:
-    img = cv.imread(input_image)[:, :, ::-1]
+for path in args.input_image:
+    img = cv.imread(path)[:, :, ::-1]
     detected_map = dwprocessor(img)
 
-    path = Path(input_image)
-    if os.path.isdir(args.input_image):
-        output_path = f"{dirpath}/{path.stem}.png"
-    else:
-        output_path = f"{path.parent}/{path.stem}_pose.png"
+    path = Path(path)
+
+    os.makedirs(f"{path.parent.parent}/pose_image", exist_ok=True)
+    output_path = f"{path.parent.parent}/pose_image/{path.name}"
     cv.imwrite(output_path, detected_map[:, :, ::-1])
